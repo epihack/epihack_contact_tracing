4ccbed2ae45dce5de1683817cba66ad3606b53a4
ksokmesa <mesa@instedd.org>
2014-12-11 12:43:24 +0300

    add map to event show

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,10 @@
+class EventsController < ApplicationController
+	before_filter :authenticate_user!
+  def index
+  	@events = Event.all
+  end
+
+  def show 
+  	@event = Event.find params[:id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
    resources :reports
 
+   resources :events
+
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
