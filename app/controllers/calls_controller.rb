bac9f328b4f0e919a6415f4a7576b79f0076561c
Martin Verzilli <mverzilli@manas.com.ar>
2014-12-11 23:26:37 +0300

    Create calls through GET (WAT?)

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,4 +1,10 @@
 class CallsController < ApplicationController
+  def index
+    c = Calls.where(call_sid: params[:CallSid]).first_or_initialize
+    c.from = params[:From]
+    c.save!
+  end
+
   def create
     c = Calls.where(call_sid: params[:CallSid]).first_or_initialize
     c.from = params[:From]
